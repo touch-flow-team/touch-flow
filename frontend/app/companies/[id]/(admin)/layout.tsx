5d177e0021b14cf592c354da72bc8837c92465fc
Xeraph <jeongdo.dev@gmail.com>
2024-02-14 14:31:19 +0900

    [Rename]: 폴더 구조 변경 - (admin) 하위로 이동

---
+++ frontend/app/companies/[id]/(admin)/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className="flex h-lvh w-full">
+        <div className="w-[200px] h-full bg-gray-200">sidebar</div>
+        <div className="flex justify-center w-full relative">{children}</div>
+      </div>
+    </>
+  );
+}
