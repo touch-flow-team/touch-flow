765b988f8ac768664eff74e1c393b5b6a7ddfab7
Xeraph <jeongdo.dev@gmail.com>
2024-02-19 14:14:54 +0900

    [Feat]: Selector 컴포넌트 생성

--- frontend/app/companies/[id]/(admin)/product/page.tsx
+++ frontend/app/companies/[id]/(admin)/product/page.tsx
@@ -1,6 +1,7 @@
 import Categories from '@/components/product/Categories';
 import Header from '@/components/product/Header';
 import ProductTable from '@/components/product/ProductTable';
+import { ICategory } from '@/components/product/Categories';
 
 export interface IResult<T> {
   page: number;
@@ -10,12 +11,30 @@ export interface IResult<T> {
   items: T[];
 }
 
+const getCategories = async () => {
+  const categories: IResult<ICategory> = await fetch(
+    'http://127.0.0.1:8090/api/collections/categorys/records',
+    { next: { tags: ['CATEGORY'], revalidate: 10 } },
+  ).then((res) => res.json());
+
+  const selector_categories = categories.items.map((category) => category.name);
+  const categories_info = categories.items.map((category) => {
+    return {
+      name: category.name,
+      id: category.id,
+    };
+  });
+  return [selector_categories, categories_info] as const;
+};
+
 export default async function Product() {
+  const [select, category_info] = await getCategories();
+
   return (
     <div className="w-full flex justify-center">
       <div className="w-[80%] flex p-10 flex-col gap-10">
-        <Header />
-        <Categories />
+        <Header categories={category_info} />
+        <Categories categories={select} />
         <ProductTable />
       </div>
     </div>
