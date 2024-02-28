4b36f7e8da0070419b6f707ee511aae436067c12
CHRIS <qkymzh123@gmail.com>
2024-02-28 11:17:20 +0900

    [Fix] 변경된 파일 구조에 맞춰 상수 임포트 주소 변경

--- frontend/app/companies/[id]/page.tsx
+++ frontend/app/companies/[id]/page.tsx
@@ -6,15 +6,15 @@ import Search from "@/components/companies/Search";
 import ShopingCartItem from "@/components/companies/ShopingCartItem";
 import StepIndicator from "@/components/companies/StepIndicator";
 import { toast } from "@/components/ui/use-toast";
-import { CookieMessageName } from "@/constants/utils";
+import { COOKIE_MESSAGE_ID } from "@/constants/constants";
 import Cookies from 'js-cookie';
 
 
 export default function Company() {
-    const message = Cookies.get(CookieMessageName);
+    const message = Cookies.get(COOKIE_MESSAGE_ID);
     if (message) {
         toast({ title: message, });
-        Cookies.remove(CookieMessageName);
+        Cookies.remove(COOKIE_MESSAGE_ID);
     }
 
     return (
